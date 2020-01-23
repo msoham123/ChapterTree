0721580c608536b86207f664619465e9dcc76ba7
aryanvichare <aryanvichare10@gmail.com>
2020-01-22 23:45:30 -0800

    delegate classes

--- lib/Screens/messageScreen.dart
+++ lib/Screens/messageScreen.dart
@@ -7,9 +7,11 @@ import 'package:mobile_app_dev/UI/widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobile_app_dev/Utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobile_app_dev/UI/message_bubble.dart';
 
 FirebaseUser loggedInUser;
 final _firestore = Firestore.instance;
+final _auth = FirebaseAuth.instance;
 
 class myMessageScreen extends StatefulWidget {
   @override
@@ -19,7 +21,6 @@ class myMessageScreen extends StatefulWidget {
 }
 
 class myMessageState extends State<myMessageScreen> {
-  final _auth = FirebaseAuth.instance;
   final messageTextController = TextEditingController();
   String messageText = "";
 
@@ -82,6 +83,10 @@ class myMessageState extends State<myMessageScreen> {
                     ),
                   ),
                   FlatButton(
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
                     onPressed: () {
                       messageTextController.clear();
                       // messageText + loggedInUser.email
@@ -91,10 +96,6 @@ class myMessageState extends State<myMessageScreen> {
                         'timestamp': FieldValue.serverTimestamp(),
                       });
                     },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
                   ),
                 ],
               ),
@@ -148,52 +149,6 @@ class MessagesStream extends StatelessWidget {
           );
         }
       },
-    );
-  }
-}
-
-class MessageBubble extends StatelessWidget {
-  final String sender;
-  final String text;
-  final bool isMe;
-  MessageBubble({this.text, this.sender, this.isMe});
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment:
-        isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            sender,
-            style: TextStyle(fontSize: 12.0, color: Colors.black54),
-          ),
-          Material(
-            borderRadius: isMe
-                ? BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                bottomLeft: Radius.circular(30.0),
-                bottomRight: Radius.circular(30.0))
-                : BorderRadius.only(
-                bottomLeft: Radius.circular(30.0),
-                bottomRight: Radius.circular(30.0),
-                topRight: Radius.circular(30.0)),
-            elevation: 5.0,
-            color: isMe ? Colors.lightBlueAccent : Colors.white,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Text(
-                '$text',
-                style: TextStyle(
-                  color: isMe ? Colors.white : Colors.black54,
-                  fontSize: 15.0,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/UI/message_bubble.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String sender;
+  final String text;
+  final bool isMe;
+  MessageBubble({this.text, this.sender, this.isMe});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+        isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            sender,
+            style: TextStyle(fontSize: 12.0, color: Colors.black54),
+          ),
+          Material(
+            borderRadius: isMe
+                ? BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0),
+                topRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
