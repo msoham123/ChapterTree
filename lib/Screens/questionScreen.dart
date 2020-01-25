47207c10e6eeb7572e27515458b38549720fcfff
aryanvichare <aryanvichare10@gmail.com>
2020-01-24 21:35:04 -0800

    add question screen

---
+++ lib/Screens/questionScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class myQuestionScreen extends StatefulWidget {
+  @override
+  _myQuestionScreenState createState() => _myQuestionScreenState();
+}
+
+class _myQuestionScreenState extends State<myQuestionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
