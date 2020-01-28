acea29c2ad4b1f9afc0f017277f727a1c04b5758
Soham Manoli <msoham123@gmail.com>
2020-01-27 21:20:41 -0800

    IOS Google Maps Functionality Added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+#import "GoogleMaps/GoogleMaps.h"
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    [GMSServices provideAPIKey: @"AIzaSyCGM1AHImb50nOWd-JWxxV5d-gfxRKUJjk"];
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
