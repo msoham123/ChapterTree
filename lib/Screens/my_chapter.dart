31a0e37704ead153b3674c534275af2e39459b12
aryanvichare <aryanvichare10@gmail.com>
2020-02-19 14:22:08 -0800

    disabled query b/c streambuilder doesn't fetch

--- lib/Screens/my_chapter.dart
+++ lib/Screens/my_chapter.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app_dev/UI/base_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobile_app_dev/Services/database.dart';
 
 import '../main.dart';
 
@@ -16,12 +17,17 @@ class myChapterScreen extends StatefulWidget {
 
 class _myChapterScreen extends State<myChapterScreen> {
   PageController _pageController;
+  FirebaseUser loggedInUser;
+  DocumentSnapshot userSnapshot;
+  DatabaseService ds = new DatabaseService();
+  String chapter = '';
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController();
     getCurrentUser();
+    _populateCurrentUser(loggedInUser);
   }
 
   @override
@@ -41,6 +47,17 @@ class _myChapterScreen extends State<myChapterScreen> {
     }
   }
 
+
+  void _populateCurrentUser(FirebaseUser user) async {
+    final FirebaseUser user = await _auth.currentUser();
+    final String userUID = user.uid.toString();
+    if(user != null) {
+      userSnapshot = await ds.getUser(userUID);
+    }
+    chapter = userSnapshot.data['chapter'].toString();
+    print(userSnapshot.data['chapter']);
+  }
+
   @override
   Widget build(BuildContext context) {
     return BaseWidget(builder: (context, sizingInformation) {
@@ -48,12 +65,12 @@ class _myChapterScreen extends State<myChapterScreen> {
         backgroundColor: MyApp.backgroundColor,
         body: StreamBuilder<QuerySnapshot>(
           stream: _firestore
-              .collection('members')
+              .collection('fbla_users')
+//             .where("chapter", isEqualTo: chapter)
               .orderBy('count', descending: true)
-              .snapshots(),
+              .snapshots(includeMetadataChanges: true),
           builder: (context, snapshot) {
-            if (!snapshot.hasData)
-              return Center(child: const Text('Loading...'));
+            if (!snapshot.hasData) return Center(child: const Text('Loading...'));
             return ListView.builder(
               itemExtent: 80.0,
               itemCount: snapshot.data.documents.length,
@@ -67,44 +84,44 @@ class _myChapterScreen extends State<myChapterScreen> {
   }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return Card(
-      color: MyApp.botBarColor,
-      child: Center(
-        child: GestureDetector(
-          onDoubleTap: () {
-            document.reference.updateData({'count': document['count'] + 1});
-          },
-          child: ListTile(
-            leading: Image.asset('./assets/images/fbla.png'),
-            title: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    document['name'],
-                    style: TextStyle(color: MyApp.blackTextColor),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    color: MyApp.blueGreyColor,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+      return Card(
+        color: MyApp.botBarColor,
+        child: Center(
+          child: GestureDetector(
+            onDoubleTap: () {
+              document.reference.updateData({'count': document['count'] + 1});
+            },
+            child: ListTile(
+              leading: Image.asset('./assets/images/fbla.png'),
+              title: Row(
+                children: <Widget>[
+                  Expanded(
                     child: Text(
-                      document['count'].toString(),
-                      style: TextStyle(
-                        color: MyApp.whiteTextColor,
-                      ),
+                      document['full_name'],
+                      style: TextStyle(color: MyApp.blackTextColor),
                     ),
                   ),
-                )
-              ],
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      color: MyApp.blueGreyColor,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        document['count'].toString(),
+                        style: TextStyle(
+                          color: MyApp.whiteTextColor,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      elevation: 10.0,
-    );
-  }
+        elevation: 10.0,
+      );
+    }
 }
