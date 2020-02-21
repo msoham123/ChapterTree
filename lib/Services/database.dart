1a63c0aee9104674f7ade417e1cd01e6a4f7138a
aryanvichare <aryanvichare10@gmail.com>
2020-02-20 22:41:34 -0800

    update database

--- lib/Services/database.dart
+++ lib/Services/database.dart
@@ -8,11 +8,13 @@ class DatabaseService {
   // Collection Reference
   final CollectionReference fbla_users = Firestore.instance.collection("fbla_users");
 
-  Future updateUserData(String fullName, int count, String chapter) async {
+  Future updateUserData(String fullName, int count, String chapter, String phone, bool isOfficer) async {
     return await fbla_users.document(uid).setData({
       'full_name': fullName,
       'count': count,
-      'chapter': chapter
+      'chapter': chapter,
+      'phone': phone,
+      'isOfficer': isOfficer
     });
   }
   Future getUser(String uid) async {
