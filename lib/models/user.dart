5c36385a02cb656d22da583157c5fbe3cc1a69dd
aryanvichare <aryanvichare10@gmail.com>
2020-03-01 19:44:56 -0800

    add helper methods

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,14 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   final String fullName;
   final int count;
   final String chapter;
 
-  User({this.fullName, this.count, this.chapter});
+  User(this.fullName, this.count, this.chapter);
 
   User.fromData(Map<String, dynamic> data)
       : fullName = data['fullName'],
         count = data['count'],
         chapter = data['chapter'];
+
+  static User fromDoc(DocumentSnapshot data) {
+    String fullName = data['fullName'];
+    int count = data['count'];
+    String chapter = data['chapter'];
+    return User(fullName, count, chapter);
+  }
 
   Map<String, dynamic> toJSON() {
     return {
