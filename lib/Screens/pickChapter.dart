0c59ec66f22603b86e04df2e7804a55147d8a5c5
aryanvichare <aryanvichare10@gmail.com>
2020-03-02 17:00:43 -0800

    add phone number field and isOfficer

--- lib/Screens/pickChapter.dart
+++ lib/Screens/pickChapter.dart
@@ -39,8 +39,8 @@ class PickChapter extends StatefulWidget {
 }
 
 class PickChapterState extends State<PickChapter> {
-  String chapter;
-  final _auth = FirebaseAuth.instance;
+  String chapter, phone;
+
   final GoogleSignIn googleSignIn = new GoogleSignIn(
     scopes: [
       'email',
@@ -51,6 +51,7 @@ class PickChapterState extends State<PickChapter> {
   TextEditingController chapterController;
   String email = "", password = "";
   bool showSpinner = false;
+  bool _officer = false;
 
   void initState() {
     super.initState();
@@ -67,160 +68,232 @@ class PickChapterState extends State<PickChapter> {
       builder: (context, sizingInformation) {
         return Scaffold(
           backgroundColor: MyApp.backgroundColor,
-          body: ModalProgressHUD(
-            inAsyncCall: showSpinner,
-            child: Stack(
-              children: <Widget>[
-                BackgroundWidget(sizingInformation),
-                SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: sizingInformation.myScreenSize.height / 10,
-                      ),
-                      Container(
-                        child: Row(
-                          children: <Widget>[
-                            SizedBox(
-                              width: sizingInformation.myScreenSize.width / 10,
-                            ),
-                            Flexible(
-                              child: Text(
-                                "Join a chapter",
-                                style: TextStyle(
-                                    color: MyApp.whiteNoChangeColor,
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w800),
-                              ),
-                              fit: FlexFit.tight,
-                              flex: 5,
-                            ),
-                            SizedBox(
-                              width: sizingInformation.myScreenSize.width / 10,
-                            ),
-                            Flexible(
-                              child: Image.asset(
-                                'assets/images/chapterTree.png',
-                                scale: 0.2,
-                              ),
-                              fit: FlexFit.tight,
-                            ),
-                            SizedBox(
-                              width: sizingInformation.myScreenSize.width / 10,
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: sizingInformation.myScreenSize.height / 7,
-                      ),
-                      Column(
+          body: Stack(
+            children: <Widget>[
+              BackgroundWidget(sizingInformation),
+              SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: sizingInformation.myScreenSize.height / 10,
+                    ),
+                    Container(
+                      child: Row(
                         children: <Widget>[
-                          Image.asset("./assets/images/pick_chapter.png"),
-                          Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.symmetric(
-                                vertical: 0,
-                                horizontal:
-                                    sizingInformation.myScreenSize.width / 10),
-                            height: sizingInformation.myScreenSize.height / 10,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                gradient: MyApp.blueGradient,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                              ),
-                              child: TextField(
-                                obscureText: false,
-                                textAlign: TextAlign.left,
-                                decoration: InputDecoration(
-                                  prefixIcon: Icon(
-                                    Icons.school,
-                                    color: Colors.white,
-                                  ),
-                                  fillColor: Colors.white,
-                                  labelText: null,
-                                  hintText: "Chapter",
-                                  labelStyle: null,
-                                  border: InputBorder.none,
-                                  focusedBorder: InputBorder.none,
-                                  enabledBorder: InputBorder.none,
-                                  errorBorder: InputBorder.none,
-                                  disabledBorder: InputBorder.none,
-                                  hintStyle: TextStyle(color: Colors.white),
-                                ),
-                                style: TextStyle(
-                                  fontSize: 25,
-                                  color: Colors.white,
-                                ),
-                                cursorWidth: 4,
-                                cursorColor: Colors.white,
-                                controller: chapterController,
-                                keyboardType: TextInputType.emailAddress,
-                                onChanged: (value) {
-                                  chapter = value;
-                                },
-                              ),
+                          SizedBox(
+                            width: sizingInformation.myScreenSize.width / 10,
+                          ),
+                          Flexible(
+                            child: Text(
+                              "Join a chapter",
+                              style: TextStyle(
+                                  color: MyApp.whiteNoChangeColor,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800),
                             ),
+                            fit: FlexFit.tight,
+                            flex: 5,
+                          ),
+                          SizedBox(
+                            width: sizingInformation.myScreenSize.width / 10,
+                          ),
+                          Flexible(
+                            child: Image.asset(
+                              'assets/images/chapterTree.png',
+                              scale: 0.2,
+                            ),
+                            fit: FlexFit.tight,
+                          ),
+                          SizedBox(
+                            width: sizingInformation.myScreenSize.width / 10,
                           ),
                         ],
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            width: sizingInformation.myScreenSize.width / 2.5,
-                            child: SimpleRoundButton(
-                              backgroundColor: MyApp.appBarColor,
-                              buttonText: Text(
-                                "NEXT",
-                                style: kAuthButtonDecoration,
+                    ),
+                    SizedBox(
+                      height: sizingInformation.myScreenSize.height / 7,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Image.asset(
+                          "./assets/images/pick_chapter.png",
+                          height: sizingInformation.myScreenSize.height / 4,
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal:
+                                  sizingInformation.myScreenSize.width / 10),
+                          height: sizingInformation.myScreenSize.height / 10,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: MyApp.blueGradient,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                            ),
+                            child: TextField(
+                              obscureText: false,
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.school,
+                                  color: Colors.white,
+                                ),
+                                fillColor: Colors.white,
+                                labelText: null,
+                                hintText: "Chapter",
+                                labelStyle: null,
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintStyle: TextStyle(color: Colors.white),
                               ),
-                              onPressed: () async {
-                                DocumentSnapshot documentSnapshot = await Firestore.instance.collection("fbla_users").document(widget.UID).get();
-
-                                int lastCount = 0;
-                                if(documentSnapshot.data != null) {
-                                  if(documentSnapshot.data['count'] != 0) {
-                                    lastCount = documentSnapshot.data['count'];
-                                  }
-                                } else {
-                                  print("Null");
-                                }
-
-                                ds = new DatabaseService(uid: widget.user.uid);
-                                ds.updateUserData(widget.user.displayName, lastCount, chapter,
-                                    widget.user.phoneNumber, false, []);
-
-
-                                if (MyApp.introSliderEnabled == true) {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          IntroScreen(sizingInformation),
-                                    ),
-                                  );
-                                  MyApp.introSliderEnabled = false;
-                                } else {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          MyDefaultPage(sizingInformation),
-                                    ),
-                                  );
-                                }
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                              cursorWidth: 4,
+                              cursorColor: Colors.white,
+                              controller: chapterController,
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                chapter = value;
                               },
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal:
+                                  sizingInformation.myScreenSize.width / 10),
+                          height: sizingInformation.myScreenSize.height / 10,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: MyApp.blueGradient,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30)),
+                            ),
+                            child: TextField(
+                              obscureText: false,
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.phone,
+                                  color: Colors.white,
+                                ),
+                                fillColor: Colors.white,
+                                labelText: null,
+                                hintText: "Phone Number",
+                                labelStyle: null,
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintStyle: TextStyle(color: Colors.white),
+                              ),
+                              style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                              cursorWidth: 4,
+                              cursorColor: Colors.white,
+                              controller: chapterController,
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                phone = value;
+                              },
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Officer : ",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w800),
+                            ),
+                            Checkbox(
+                              value: _officer,
+                              onChanged: (bool newValue) {
+                                print(newValue);
+                                setState(() {
+                                  _officer = newValue;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          width: sizingInformation.myScreenSize.width / 2.5,
+                          child: SimpleRoundButton(
+                            backgroundColor: MyApp.appBarColor,
+                            buttonText: Text(
+                              "NEXT",
+                              style: kAuthButtonDecoration,
+                            ),
+                            onPressed: () async {
+                              DocumentSnapshot documentSnapshot =
+                                  await Firestore.instance
+                                      .collection("fbla_users")
+                                      .document(widget.UID)
+                                      .get();
+
+                              int lastCount = 0;
+                              if (documentSnapshot.data != null) {
+                                if (documentSnapshot.data['count'] != 0) {
+                                  lastCount = documentSnapshot.data['count'];
+                                }
+                              } else {
+                                print("Null");
+                              }
+
+                              ds = new DatabaseService(uid: widget.user.uid);
+                              ds.updateUserData(
+                                  widget.user.displayName,
+                                  lastCount,
+                                  chapter,
+                                  phone,
+                                  false, []);
+
+                              if (MyApp.introSliderEnabled == true) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        IntroScreen(sizingInformation),
+                                  ),
+                                );
+                                MyApp.introSliderEnabled = false;
+                              } else {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        MyDefaultPage(sizingInformation),
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
